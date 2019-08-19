addeffb9d75d316f2d2fe89b7fd1694f5acf6fe3
Mark French <markedfrench@gmail.com>
2019-08-19 16:20:43 -0500

    Initial commit, Routes are set up.

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,11 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+          <Route path="/" exact component={MovieList} />
+          <Route path="/movies/:id" component={Movie} />
+       
+      </Switch>
     </div>
   );
 };
